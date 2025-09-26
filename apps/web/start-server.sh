c6ff7b61a13bcb4f784c6854e03ecd6d6896a934
lolmach <lolmach@lolmachs-MacBook-Air.local>
2025-09-26 16:44:28 -0700

    Force serve to bind to 0.0.0.0 with HOST environment variable
    - Set HOST=0.0.0.0 environment variable in start script
    - Revert back to serve package instead of http-server
    - This should force serve to bind to all network interfaces instead of localhost
    - Railway needs services to bind to 0.0.0.0 to be externally accessible

--- apps/web/start-server.sh
+++ apps/web/start-server.sh
@@ -2,8 +2,8 @@
 
 # Simple wrapper script to start serve and ignore any extra arguments
 # Railway tends to append extra arguments that serve doesn't understand
-# -s flag enables Single Page Application mode (rewrites 404s to index.html)
-# -p flag specifies port (serve will bind to 0.0.0.0 by default)
+# Try using environment variables to force binding to all interfaces
 
-echo "Starting serve on port ${PORT:-5173}"
+echo "Starting serve on 0.0.0.0:${PORT:-5173}"
+export HOST=0.0.0.0
 exec pnpm exec serve -s dist -p ${PORT:-5173}
